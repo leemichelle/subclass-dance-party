1c73bd4b559451270ab2153345d418606cca682a
leemichelle <mlee749@gmail.com>
2018-09-24 17:57:35 -0700

    FINALLY got the blinky dancer class converted to PC style
    TODO:
    Get tests passing
    Add new dancers

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,21 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  const blinker = new BlinkyDancer(top, left, timeBetweenSteps);
+  return blinker;
+};
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.oldStep = Dancer.prototype.step;
+};
 
-  var oldStep = blinkyDancer.step;
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
 
-  return blinkyDancer;
+BlinkyDancer.prototype.step = function() {
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.oldStep();
+  this.$node.toggle();
 };
